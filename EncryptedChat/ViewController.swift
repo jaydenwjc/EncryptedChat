3478a5de1068bb04665d9747aa19d459045b6969
Jayden Wang <jaydenwjc@gmail.com>
2022-12-12 23:06:15 -0500

    Initial Commit

---
+++ EncryptedChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EncryptedChat
+//
+//  Created by Jichu Wang on 12/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
